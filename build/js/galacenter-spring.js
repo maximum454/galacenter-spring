11f523cefebd593cbed95126c6ccb7e71caff365
Maxim Karachyov <karachyov.m@gmail.com>
2022-02-20 22:27:09 +0300

    parallax fix

--- build/js/galacenter-spring.js
+++ build/js/galacenter-spring.js
@@ -52,12 +52,16 @@ function getScrollY()
 }
 
 $(function (){
-    const bodyHeight = $('body').outerHeight()/2;
+    const bodyHeight = $('.main').outerHeight()/2;
     $(document).on('scroll', function (e){
+        let scrollT = $(window).scrollTop();
+
         if ( $(window).scrollTop() >= bodyHeight ){
             $('body').addClass('parallax')
+            $('body').css({'background-position': 'bottom'});
         }else{
             $('body').removeClass('parallax')
+            $('body').css({'background-position': '0 -'+(scrollT/3)+'px'});
         }
     })
 })
